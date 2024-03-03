74809343eb52ddd652dd344789701d4edafa71b5
unknown <tubesniper@gmail.com>
2024-03-03 22:34:16 +0200

    fix center attendence

--- src/api/classroom/attempts.api.ts
+++ src/api/classroom/attempts.api.ts
@@ -1,8 +1,12 @@
 import {WithProgressArgs, api, apiConfig} from '../api';
 
-export const getTestAttempts = async (withProgress?: WithProgressArgs , page : number = 1) =>
+export const getTestAttempts = async (
+  withProgress?: WithProgressArgs,
+  page: number = 1,
+  perPage = 1000,
+) =>
   await api.get<TestAttemptsResponse>(
-    `/attendence/offline/fetch/test-attempts?page=${page}`,
+    `/attendence/offline/fetch/test-attempts?page=${page}&perPage=${perPage}`,
     {
       onDownloadProgress: withProgress?.onDownloadProgress,
     },
